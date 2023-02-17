f877a4e61681bbb101eced3550bf52e1ee6acb61
tijeffrey <112028948+tijeffrey@users.noreply.github.com>
2023-02-16 22:47:18 -0500

    first commit

--- 7 Kyu/Vowel Count.js
+++ 7 Kyu/Vowel Count.js
@@ -8,22 +8,13 @@
 // The input string will only consist of lower case letters and/or spaces.
 
 function getCount(str) {
-    let count = 0 
-    let vowels = 'aeiouAEIOU'
+  
+    let vowels = ["a", "e", "i", "o", "u"];
+    let count = 0;
+    
     for (let i = 0; i < str.length; i++){
       for (let j = 0; j < vowels.length; j++){
         if (str[i] === vowels[j]) count++
-      }
-    }
-    return count
-  }
-
-  function getCount(str) {
-    let y = str.split('')
-    let count = 0 
-    for (let i = 0; i < y.length; i++){
-      if (y[i] === 'a' || y[i] === 'e' || y[i] === 'i' || y[i] === 'o' || y[i] === 'u'){
-        count++
       }
     }
     return count
