1fc88e163736c32fa41874b3bf7597c51377b363
tijeffrey <112028948+tijeffrey@users.noreply.github.com>
2023-02-22 23:40:36 -0500

    first commit

--- 6 Kyu/Highest Scoring Word.js
+++ 6 Kyu/Highest Scoring Word.js
@@ -35,3 +35,8 @@ function high(x){
     }
     return highestScoringWord 
   }
+
+  function high(x){
+    let as =  x.split(' ').map(x => [...x].reduce((a, b) => a+b.charCodeAt(0) - 96, 0))
+    return x.split(' ')[as.indexOf(Math.max(...as))]
+  }
