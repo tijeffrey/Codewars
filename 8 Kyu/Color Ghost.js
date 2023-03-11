b654586fa97a76e1e1adbca701b29e426889c7ca
tijeffrey <112028948+tijeffrey@users.noreply.github.com>
2023-03-11 16:22:02 -0500

    first commit

---
+++ 8 Kyu/Color Ghost.js
@@ -0,0 +1,19 @@
+// Color Ghost
+
+// DESCRIPTION:
+// Color Ghost
+// Create a class Ghost
+
+// Ghost objects are instantiated without any arguments.
+
+// Ghost objects are given a random color attribute of "white" or "yellow" or "purple" or "red" when instantiated
+
+// ghost = new Ghost();
+// ghost.color //=> "white" or "yellow" or "purple" or "red"
+
+var Ghost = function() {
+  let color = ["red", "white", "yellow", "purple"]
+  let colorIndex = Math.floor(Math.random()* (color.length))
+  
+  this.color = color[colorIndex]
+};
