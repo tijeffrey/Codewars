b8bfcace46061462388da502b773cd596fcd5b2a
tijeffrey <112028948+tijeffrey@users.noreply.github.com>
2023-03-13 23:48:41 -0400

    first commit

--- 7 Kyu/Numbers to Objects.js
+++ 7 Kyu/Numbers to Objects.js
@@ -16,3 +16,13 @@ function numObj(s){
         return { [n]: String.fromCharCode(n)};
     });
 }
+
+function numObj(s){
+    const arr = []
+    for(i=0; i<s.length;i++){
+      const obj = {}
+      obj[s[i]] = String.fromCharCode(s[i])
+      arr.push(obj)
+    }
+    return arr
+  }
